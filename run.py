3801d25371b605024df68aa9a1a3ecb45407fdb2
Michal Srb <michal@redhat.com>
2018-01-30 10:32:14 +0100

    More refactoring

--- run.py
+++ run.py
@@ -1,9 +1,75 @@
 import json
 import subprocess
 import datetime
+from collections import OrderedDict
+
 from utils import get_first_sentence, guess_package_name, get_versions
 from output import generate_yaml
 from cve import CVE, cpe_is_app, extract_vendor_product_version
+
+
+def is_older_than(cve, days):
+    """Check if the CVE was last modified no longer than `days` ago."""
+    now = datetime.datetime.now()
+    age = now.date() - cve.last_modified_date.date()
+    return age.days > days
+
+
+def get_vendor_product_versions(cve):
+    """Extract vendor, product and versions."""
+    vendor = []
+    product = []
+    cpe_versions = set()
+    for conf in cve.configurations:
+        if not cpe_is_app(conf):
+            # not an app, skipping...
+            continue
+
+        ven, prod, ver = extract_vendor_product_version(conf)
+        vendor.append(ven)
+        product.append(prod)
+        cpe_versions.add(ver)
+        if cve.configurations[conf]:
+            cpe_versions.add(cve.configurations[conf]['version'])
+
+    return vendor, product, cpe_versions
+
+
+def get_package_name_candidates(cve):
+    """Try to identify possible package names in the CVE's description."""
+    pkg_name_candidates = set()
+    for description in cve.descriptions:
+        first_sentence = get_first_sentence(description)
+        names = guess_package_name(first_sentence)
+        pkg_name_candidates.update(set(names))
+    return pkg_name_candidates
+
+
+def construct_lucene_query(vendor, product):
+    """Construct lucene query for given vendor and product."""
+    query_template = 'product:( {product} )  AND  vendor:( {vendor} )'
+    p = ' '.join(product).replace(':', ' ')
+    v = ' '.join(vendor).replace(':', ' ')
+    query = query_template.format(product=p, vendor=v)
+
+    return query
+
+
+def run_cpe2pkg(query):
+    """Run cpe2pkg tool with given query."""
+    cpe2pkg_output = subprocess.check_output('java -jar target/cpe2pkg.jar "' + query + '"',
+                                             shell=True,
+                                             universal_newlines=True)
+    cpe2pkg_lines = cpe2pkg_output.split('\n')
+    results = []
+
+    for line in cpe2pkg_lines:
+        if not line:
+            continue
+        score, ga = line.split()
+        results.append({'ga': ga, 'score': score})
+
+    return results
 
 
 def run():
@@ -16,64 +82,39 @@ def run():
             print('---')
             print(cve.cve_id)
 
-            now = datetime.datetime.now()
-            age = now.date() - cve.last_modified_date.date()
-            if not cve.last_modified_date or age.days >= 1:
-                # not modified today/yesterday, skipping...
+            if is_older_than(cve, 1):
+                # the CVE is too old, skipping...
                 continue
 
             if not cve.configurations:
                 # vulnerability still under analysis, skipping...
                 continue
 
-            vendor = []
-            product = []
-            version = set()
-            for conf in cve.configurations:
-                if not cpe_is_app(conf):
-                    continue
+            vendor, product, cpe_versions = get_vendor_product_versions(cve)
+            pkg_name_candidates = get_package_name_candidates(cve)
 
-                ven, prod, ver = extract_vendor_product_version(conf)
-                vendor.append(ven)
-                product.append(prod)
-                version.add(ver)
-                if cve.configurations[conf]:
-                    version.add(cve.configurations[conf]['version'])
+            product = list(OrderedDict.fromkeys(product + list(pkg_name_candidates)))
+            vendor = list(OrderedDict.fromkeys(vendor))
 
-            pkg_name_candidates = set()
-            for description in cve.descriptions:
-                first_sentence = get_first_sentence(description)
-                names = guess_package_name(first_sentence)
-                pkg_name_candidates.update(set(names))
-
-            product += list(pkg_name_candidates)
             if not product or not vendor:
                 continue
 
-            query_template = 'product:( {product} )  AND  vendor:( {vendor} )'
-            p = ' '.join(product).replace(':', ' ')
-            v = ' '.join(vendor).replace(':', ' ')
-            query = query_template.format(product=p, vendor=v)
+            query = construct_lucene_query(vendor, product)
             print('Query: ' + query)
 
-            cpe2pkg_output = subprocess.check_output('java -jar target/cpe2pkg.jar "' + query + '"',
-                                                     shell=True,
-                                                     universal_newlines=True)
-            cpe2pkg_lines = cpe2pkg_output.split('\n')
-            results = []
+            results = run_cpe2pkg(query)
 
-            for line in cpe2pkg_lines:
-                if not line:
-                    continue
-                score, ga = line.split()
-                results.append({'ga': ga, 'score': score})
-
+            # try to exclude false positives
+            # winner is simply the first match we found
             winner = None
             for result in results:
                 ga = result['ga']
+
                 upstream_versions = get_versions(ga)
-                affected = version & set(upstream_versions)
-                if affected:
+
+                # check if at least one version mentioned in the CVE exists for given groupId:artifactId;
+                # if not, this is a false positive
+                if cpe_versions & set(upstream_versions):
                     print('Hit!')
                     result['versions'] = upstream_versions
                     winner = result
