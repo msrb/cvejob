99209c6d1c07d5964919359215161ee29fe55258
Michal Srb <michal@redhat.com>
2018-02-09 08:01:16 +0100

    Add ability to download any NVD data feed, not just the recent

--- get_nvd_recent.sh
+++ get_nvd_recent.sh
@@ -1,10 +1,17 @@
 #!/usr/bin/env bash
 
+# Parameters:
+# $1 - data feed to download; example: 2012
+#      default: recent
+
 set -e
 set -x
 
-url='https://static.nvd.nist.gov/feeds/json/cve/1.0/nvdcve-1.0-recent.json.gz'
+feed='recent'
+[ -n "$1" ] && feed=$1
+
+url="https://static.nvd.nist.gov/feeds/json/cve/1.0/nvdcve-1.0-$feed.json.gz"
 
 curl -sL ${url} | gunzip - > nvdcve.json
 
-echo "Successfully downloaded recent NVD data feed."
+echo "Successfully downloaded $feed NVD data feed."
